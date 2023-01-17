4e540da46c63b21c7aebb5c2f9b8ccdee86afe8a
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-17 17:06:53 +0100

    add method to define relation between two models Project and Technology

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProjectSeeder::class,
             TypesTableSeeder::class,
+            TechnologySeeder::class,
         ]);
     }
 }
